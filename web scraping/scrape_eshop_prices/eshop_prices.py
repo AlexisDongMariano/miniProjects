8286c78ae795b39d0b0b102315bf112e9e11ba52
unknown <alexisdongmariano@gmail.com>
2021-03-20 22:13:31 -0700

    added py file

--- web scraping/scrape_eshop_prices/eshop_prices.py
+++ web scraping/scrape_eshop_prices/eshop_prices.py
@@ -3,10 +3,32 @@ import requests
 
 
 BASE_DIR = os.path.dirname(__file__)
-URL = 'https://eshop-prices.com/games/on-sale?direction=desc&sort_by=score'
+
+
+
+
+def generate_path(data_folder):
+    return os.path.join(BASE_DIR, data_folder)
+
+
+def get_html_text(url, headers, path, fname, save=False):
+    fpath = f'{path}{fname}.html'
+    r = requests.get(url, headers=headers)
+    if 200 <= r.status_code <= 299:
+        # if save:
+        #     with open(fpath, 'w') as f:
+        #         f.write(r.text)
+        return r.text
+    else:
+        print(r.raise_for_status())
+
+
+url = 'https://eshop-prices.com/games/on-sale?direction=desc&sort_by=score'
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Safari/537.36'}
-r = requests.get(URL, headers=headers)
+data_folder = 'data'
+fname = 'eshop_prices'
 
+path = generate_path(data_folder)
+html_text = get_html_text(url, headers, path, fname, True)
+print(html_text)
 
-print(r.status_code)
-print(r.text)
